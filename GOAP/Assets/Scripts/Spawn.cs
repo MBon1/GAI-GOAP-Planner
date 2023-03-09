a2824423129616464c0134c2741ceba59f36a2a7
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2023-03-09 16:46:03 -0500

    Final Attempt

--- GOAP/Assets/Scripts/Spawn.cs
+++ GOAP/Assets/Scripts/Spawn.cs
@@ -10,10 +10,10 @@ public class Spawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < numPatients; i++)
+        /*for (int i = 0; i < numPatients; i++)
         {
             Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
-        }
+        }*/
 
         Invoke("SpawnPatient", 5);
     }
